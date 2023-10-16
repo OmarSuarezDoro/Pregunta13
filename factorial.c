8d76b5dcb203f53de75720a1ac0d91038a3ff284
OmarSuarezDoro <alu0101483474@ull.edu.es>
2023-10-16 08:17:18 +0100

    Rama imperativo terminada

---
+++ factorial.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int factorial(int n) {
+  if (n == 0) {
+    return 1;
+  } else {
+    return n * factorial(n-1);
+  }
+}
+
+int main() {
+  int n;
+  printf("Ingrese un número entero positivo: ");
+  scanf("%d", &n);
+  printf("El factorial de %d es %d\n", n, factorial(n));
+  return 0;
+}
